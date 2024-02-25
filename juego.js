da2bfcaa58c944717bb998cdbcb044ea45c4f1b0
Daihana <coronelfatima94@gmail.com>
2024-02-25 16:34:31 -0300

    final

---
+++ juego.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const inicio = document.getElementById("enter");
+    inicio.addEventListener("click", function () {
+        location.href = "juego2.html";
+    })
+    const enter = document.getElementById("conectar");
+    enter.addEventListener("click", function () {
+        location.href = "juego.html";
+    })
+})
+
